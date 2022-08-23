094d0a1ce1bfd030b8781eda6fbf428d18acbed4
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-23 09:30:29 -0700

    Adv Task complete (original version)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,10 +4,10 @@ file { '/etc/ssh/ssh_config':
   ensure => 'present',
 }
 
-#exec { 'IdentityFile'
-#    command => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-#    provider => 'shell',
-#}
+exec { 'IdentityFile'
+    command => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+    provider => 'shell',
+}
 
 exec { 'PasswordAutentication'
     command => '/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config'
