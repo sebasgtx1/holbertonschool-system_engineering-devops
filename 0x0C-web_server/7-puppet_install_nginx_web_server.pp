90a6f55169227d3060c74ec7cf74e56126c51be3
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-26 12:51:25 -0700

    Adv task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,29 @@
 # Install Nginx web server (w/ Puppet)
-exec { 'Nginx_install':
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello wolrd" > /var/www/html/index.html ; sudo sed -i "s/server_name _; sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4\&ab_channel=NyanCat permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
-  provider => 'shell',
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_redirect'],
+}
+
+exec { 'add_redirect':
+  provider    => shell,
+  command     => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4\&ab_channel=NyanCat permanent;/" /etc/nginx/sites-available/default',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'add_index':
+  provider    => shell,
+  command     => 'echo "Hello World" > /var/www/html/index.html',
+  before      => Exec['add_redirect'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
