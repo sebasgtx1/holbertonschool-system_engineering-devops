c8660e1d41dfef683da9d99a6d5d0f6487d23388
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-09 17:43:54 -0500

    [ADD]: Task 0 complete

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = '{}users/{}'.format(url, sys.argv[1])
+    r = requests.get(user)
+    r_json = r.json()
+    print("Employee {} is done with tasks".format(r_json.get('name')), end="")
+    r = requests.get(url + 'todos', params={'userId': sys.argv[1]})
+    tasks = r.json()
+    task_list = []
+    for task in tasks:
+        if task.get('completed'):
+            task_list.append(task)
+
+    print("({}/{}):".format(len(task_list), len(tasks)))
+    for task in task_list:
+        print("\t {}".format(task.get("title")))
